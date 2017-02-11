8e4586f147d5b831a91a96981feda3d0a02becf4
Jinesh220 <jsubhashchand31@student.mes.ac.in>
2017-02-11 14:25:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+    
+  res.send(counter.toString());
 });
 app.get('/article-one',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
