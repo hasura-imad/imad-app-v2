152e43a20d127a207d2865687914acd6c681d257
Jinesh220 <jsubhashchand31@student.mes.ac.in>
2017-02-11 14:38:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick=function() {
+    counter=counter +1;
+  var span=document.getElementById('count');  
+  span.innerHTML=counter.toString();
+};
