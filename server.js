11620c1c03491b90ed1d07c301548a3a5bf38e8b
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-11 19:10:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/article-one.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/ui/article-two.html', function (req, res) {
+  res.send('article-two.html shall be served here, in the future');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
