f6da9cecd499d66869f747e73cc4c4c306c4f5ab
selfdirectedlearner <p3learn@gmail.com>
2017-02-11 16:22:33 +0530

    edited server.js to include new image files and their locations

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/images/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'images', 'madi.png'));
 });
 
+app.get('/images/chaplin.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'images', 'chaplin.png'));
+});
+
+app.get('/images/xkcd52.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'images', 'xkcd52.jpg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
