73a4693400236bd9d6f5479ea38823948873cdb2
YogeshSingla <yogeshsingla131@gmail.com>
2017-02-11 16:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,18 +33,63 @@ var article1 ={
         
         </div>
     `
+};
+
+
+function createTemplate(data){
+    title   =   data.title;
+    date    =   data.date;
+    heading =   data.heading;
+    content =   data.content;
+var  htmlTemplate =   `
+<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name="viewport"   content="width=device-width,   initial-scale=1"/>
+        <link href="/ui/style.css"   rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        
+        <div>
+        <a href="/">Home</a>    
+        </div>
+        
+        <hr/>
+        
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+            ${content}  
+        </div>
+        
+        </div>
+    </body>
+</html>
+';
+
+return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+/*
 app.get('/article-one',function(req,res) {
     res.send('Article one requested and will be served here');
 });
+*/
 
 app.get('/ui/article1.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.send(createTemplate(article1));
 });
 app.get('/ui/article2.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
