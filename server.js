adee293aa5661aa2a6aea4cbbc9f08f5b0e7f6ae
YogeshSingla <yogeshsingla131@gmail.com>
2017-02-11 15:55:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get('/article-one',function(req,res) {
     res.send('Article one requested and will be served here');
 });
 
+
+app.get('/article-two',function(req,res) {
+    res.send('Article 2 requested and will be served here');
+});
+
+
+app.get('/article-three',function(req,res) {
+    res.send('Article 3 requested and will be served here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
