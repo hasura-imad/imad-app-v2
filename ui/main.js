2dd44d715fef7deb4e38486fc25faf8797235908
Jinesh220 <jsubhashchand31@student.mes.ac.in>
2017-02-11 14:58:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,20 @@ var button=document.getElementById('counter');
 var counter=0;
 
 button.onclick=function() {
-    counter=counter +1;
-  var span=document.getElementById('count');  
-  span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    request.onreadytstatechange=function() {
+        if(request.readyState==XMLHttpRequest.Done) {
+            if(request.status==2000) {
+                var counter=request.responseText;
+                 var span=document.getElementById('count');  
+                span.innerHTML=counter.toString();
+            }
+        }
+
+   
+ 
+};
+request.open('GET',"http://jinesh220.imad.hasura-app.io/counter",true);
+request.send(null);
+
 };
