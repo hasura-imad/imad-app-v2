554370d7cdbc890b499c42ac61c014fdfd8b029d
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-12 16:12:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,8 @@
 console.log('Loaded!');
 var imgs = document.getElementsByClassName('img-medium');
 var imgsBtn = document.getElementById('imgsBtn');
+var imgCount = document.getElementById('imgCount');
+document.onload.imgCount.innerHTML = "4";
 
 imgs[0].onclick = function(){
         imgs[0].style.display = 'none';
