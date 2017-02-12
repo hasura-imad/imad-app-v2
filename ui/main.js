5e56ce3e7f140bce0953070c937d904cf889bb7d
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-12 16:04:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,30 @@
 console.log('Loaded!');
 var imgs = document.getElementsByClassName('img-medium');
+var imgsBtn = document.getElementById('imgsBtn');
 
 imgs[0].onclick = function(){
         imgs[0].style.display = 'none';
+        imgsBtn.style.display = "block";
 };
 
 imgs[1].onclick = function(){
         imgs[1].style.display = 'none';
+        imgsBtn.style.display = "block";
 };
 
 imgs[2].onclick = function(){
         imgs[2].style.display = 'none';
+        imgsBtn.style.display = "block";
 };
 
 imgs[3].onclick = function(){
         imgs[3].style.display = 'none';
+        imgsBtn.style.display = "block";
 };
 
 function show(){
     for(var i = 0; i < imgs.length; i++){
         imgs[i].style.display = "inline";
     }
+    imgsBtn.style.display = "none";
 }
