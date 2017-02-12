27fd09864be878c623e2e9e594788e048b1e224b
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-12 15:57:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,17 +2,17 @@ console.log('Loaded!');
 var imgs = document.getElementsByClassName('img-medium');
 
 imgs[0].onclick = function(){
-        imgs[1].style.display = 'none';
+        imgs[0].style.display = 'none';
 };
 
 imgs[1].onclick = function(){
-        imgs[2].style.display = 'none';
+        imgs[1].style.display = 'none';
 };
 
 imgs[2].onclick = function(){
-        imgs[3].style.display = 'none';
+        imgs[2].style.display = 'none';
 };
 
 imgs[3].onclick = function(){
-        imgs[0].style.display = 'none';
+        imgs[3].style.display = 'none';
 };
