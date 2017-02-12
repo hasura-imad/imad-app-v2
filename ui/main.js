61d30978de13d63cce972ec4a31a9f3ea33066c8
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-12 16:18:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ imgs[0].onclick = function(){
         imgsBtn.style.display = "inline";
         
         imgShown--;
-        imgCount.innerHTML = "Images Visible: " + imgShown + "//4";
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 };
 
 imgs[1].onclick = function(){
@@ -18,7 +18,7 @@ imgs[1].onclick = function(){
         imgsBtn.style.display = "inline";
         
         imgShown--;
-        imgCount.innerHTML = "Images Visible: " + imgShown + "//4";
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 };
 
 imgs[2].onclick = function(){
@@ -26,7 +26,7 @@ imgs[2].onclick = function(){
         imgsBtn.style.display = "inline";
         
         imgShown--;
-        imgCount.innerHTML = "Images Visible: " + imgShown + "//4";
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 };
 
 imgs[3].onclick = function(){
@@ -34,7 +34,7 @@ imgs[3].onclick = function(){
         imgsBtn.style.display = "inline";
         
         imgShown--;
-        imgCount.innerHTML = "Images Visible: " + imgShown + "//4";
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 };
 
 function show(){
@@ -44,5 +44,5 @@ function show(){
     imgsBtn.style.display = "none";
     
     imgShown = 4;
-    imgCount.innerHTML = "Images Visible: " + imgShown + "//4";
+    imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 }
