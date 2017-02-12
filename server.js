13af82279f37e863105c700a3840beb32011a792
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-12 07:54:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate('articleOne'));
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
