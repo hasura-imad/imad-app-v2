fe300563179f04bc12bb59b3622e26a9cfac2a23
selfdirectedlearner <p3learn@gmail.com>
2017-02-13 11:01:21 +0530

    copied back style.css and main.js, since imad console giving error

---
+++ ui/main.js
@@ -0,0 +1,49 @@
+//client side javascript code for index.html
+console.log('Loaded!');
+var imgs = document.getElementsByClassName('img-medium');
+var imgsBtn = document.getElementById('imgsBtn');
+var imgCount = document.getElementById('imgCount');
+var imgShown = 4;
+imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+
+imgs[0].onclick = function(){
+        imgs[0].style.display = 'none';
+        imgsBtn.style.display = "inline";
+        
+        imgShown--;
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+};
+
+imgs[1].onclick = function(){
+        imgs[1].style.display = 'none';
+        imgsBtn.style.display = "inline";
+        
+        imgShown--;
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+};
+
+imgs[2].onclick = function(){
+        imgs[2].style.display = 'none';
+        imgsBtn.style.display = "inline";
+        
+        imgShown--;
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+};
+
+imgs[3].onclick = function(){
+        imgs[3].style.display = 'none';
+        imgsBtn.style.display = "inline";
+        
+        imgShown--;
+        imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+};
+
+function show(){
+    for(var i = 0; i < imgs.length; i++){
+        imgs[i].style.display = "inline";
+    }
+    imgsBtn.style.display = "none";
+    
+    imgShown = 4;
+    imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
+}
