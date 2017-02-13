ad0395c064d8d1ad48e7acb7ab8097cd29520189
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-13 11:21:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ function createTemplate(data){
             <head>
                 <title>${title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link href="/ui/style.css" rel="stylesheet" />
+                <link href="/styles/style.css" rel="stylesheet" />
             </head>
             <body>
                 <div class="container">
