d511f36eb8d7a157a203f6fc8ea5a76e50e9702d
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-13 10:52:07 +0530

    [imad-console] Updates ui/profile.js

--- ui/profile.js
+++ ui/profile.js
@@ -1,1 +1,25 @@
+//Counter endpoint code is connected to the button click event, using XMLHttpRequest
+var counterBtn = document.getElementById('counter');
 
+counterBtn.onclick = function(){
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                
+                //Render the variable in the correct span
+                var countSpan = document.getElementById('count');
+                countSpan.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    
+    //Make the request to /counter endpoint
+    request.open('GET', 'http://selfdirectedlearner.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
