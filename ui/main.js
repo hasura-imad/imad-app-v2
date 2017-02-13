27aa074e5ce876c891f829243e18e878b2bc3b52
selfdirectedlearner <selfdirectedlearner@users.noreply.github.com>
2017-02-13 10:50:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,3 @@
-//Counter endpoint code is connected to the button click event, using XMLHttpRequest
-var counterBtn = document.getElementById('counter');
-
-counterBtn.onclick = function(){
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                
-                //Render the variable in the correct span
-                var countSpan = document.getElementById('count');
-                countSpan.innerHTML = counter.toString();
-            }
-        }
-    };
-    
-    
-    //Make the request to /counter endpoint
-    request.open('GET', 'http://selfdirectedlearner.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
-
 //client side javascript code for index.html
 console.log('Loaded!');
 var imgs = document.getElementsByClassName('img-medium');
@@ -74,3 +47,30 @@ function show(){
     imgShown = 4;
     imgCount.innerHTML = "Images Visible: " + imgShown + "/4";
 }
+
+
+//Counter endpoint code is connected to the button click event, using XMLHttpRequest
+var counterBtn = document.getElementById('counter');
+
+counterBtn.onclick = function(){
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                
+                //Render the variable in the correct span
+                var countSpan = document.getElementById('count');
+                countSpan.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    
+    //Make the request to /counter endpoint
+    request.open('GET', 'http://selfdirectedlearner.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
