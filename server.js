33436f3d247bc9f27226b171ab68646d2ac077f5
anutony <aisavaria@gmail.com>
2017-02-18 16:11:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
