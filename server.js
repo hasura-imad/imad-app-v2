eb8da11eedb689cd14d98b573c59d80d2ad294c9
Bhargav Vasist <bhargavclick@gmail.com>
2017-02-19 12:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var art1 = {
+    title: "This is Article Number one",
+    heading: "Hello, Welcome to Article Number One",
+    body: `This article deals with the sad state of MMORPGs in 2017 and how we can make MMORPGs great again!
+            Why does the game genre that once ruled the entire video game industry, now lie in a trashcan with other dead genres like Spy Thrillers and Side Scrollers?
+            What happened man...`,
+    linkstuff: `<a href='/'>Home</a>
+            <a href='/post1.html'>Post</a>
+            <a href='/file1.html'>File</a>`
+};
+
+function createTemplate(data){
+
+var title= data.title;
+var heading= data.heading;
+var body= data.body;
+var linkstuff= data.linkstuff;
+    
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1 />
+            <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="cont">
+        <div>
+            ${linkstuff}
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <hr/>
+        <hr/>
+        <p>
+            ${body}
+        </p>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+    
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -18,7 +68,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article1.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'content', 'article1.html'));
+  res.send(createTemplate(art1));
 });
 
 app.get('/file1.html', function (req, res) {
