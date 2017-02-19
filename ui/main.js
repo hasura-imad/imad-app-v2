73f4b43b1656f170306a6fdc7a3515bcbc5f9f77
Bhargav Vasist <bhargavclick@gmail.com>
2017-02-19 18:53:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,5 @@ var element= document.getElementById('butt');
 
 element.onClick= function pop() {
     alert('MMORPG stands for Massively Multiplayer Online Role Playing Game(ya dummy)');
+    element.style.marginTop = '80px';
 };
