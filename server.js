e6a1c9866db63cfe946015e13c54026b2b48c62b
Bhargav Vasist <bhargavclick@gmail.com>
2017-02-19 13:03:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var art1 = {
+var objs = {
+    'article1.html': {
     title: "This is Article Number one",
     heading: "Hello, Welcome to Article Number One",
     body: `This article deals with the sad state of MMORPGs in 2017 and how we can make MMORPGs great again!
@@ -15,6 +15,32 @@ var art1 = {
     linkstuff: `<a href='/'>Home</a>
             <a href='/post1.html'>Post</a>
             <a href='/file1.html'>File</a>`
+},
+   'post1.html': {
+       title: "Post One | What happened la",
+    heading: "Hello, Welcome to Article Number One",
+    body: `This article deals with the sad state of MMORPGs in 2017 and how we can make MMORPGs great again!
+            Why does the game genre that once ruled the entire video game industry, now lie in a trashcan with other dead genres like Spy Thrillers and Side Scrollers?
+            What happened man...`,
+    linkstuff: `<a href='/'>Home</a>
+           <a href='/article1.html'>Article</a>
+           <a href='/file1.html'>File</a>`
+       
+   },
+   'file1.html': {
+       title: "File One | MMORPG History",
+    heading: "This file contains a brief note of the important MMORPGs that have helped shape the history of video gaming industry forever.",
+    body: `<ol>
+             <li>World of Warcraft</li>   
+               <li>Runescape</li> 
+               <li> Maplestory</li>
+                </ol>`,
+    linkstuff: `<a href='/'>Home</a>
+            <a href='/article1.html'>Article</a>
+            <a href='/post1.html'>Post</a>`
+       
+   }
+
 };
 
 function createTemplate(data){
@@ -65,11 +91,12 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article1.html', function (req, res) {
-  res.send(createTemplate(art1));
+app.get('/:post', function (req, res) {
+    var post = req.params.post;
+  res.send(createTemplate(objs[post]));
 });
 
-app.get('/file1.html', function (req, res) {
+/*app.get('/file1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'content', 'file1.html'));
 });
 
@@ -77,7 +104,7 @@ app.get('/post1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'content', 'post1.html'));
 });
 
-
+*/
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
