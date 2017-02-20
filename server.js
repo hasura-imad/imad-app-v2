f3bacbf5d40261e6943dbcb59bcd29732ec45095
iamshristy <sahuwalashristy@gmail.com>
2017-02-20 22:38:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req, res){
+app.get('/article-one',function (req, res) {
     res.send('Article-one requested and will be served here');
 });
 
-app.get('/article-two',function (req, res){
+app.get('/article-two',function (req, res) {
     res.send('Article-two requested and will be served here');
 });
 
-app.get('/article-three',function (req, res){
+app.get('/article-three',function (req, res) {
     res.send('Article-three requested and will be served here');
 });
 
@@ -25,7 +25,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/motivating-your-employees.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
