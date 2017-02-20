6194f6f1a47c275b4b5923b075c37afd0c78da5e
anutony <aisavaria@gmail.com>
2017-02-20 16:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
@@ -60,6 +59,7 @@ var articleOne = {
      </html>`;
      
     return htmltemplate
+    
     }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
