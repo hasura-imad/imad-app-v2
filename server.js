72e382e1e4a2252b5e8d8e0a4ea072e8bd15e69e
anutony <aisavaria@gmail.com>
2017-02-20 15:38:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,60 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
 var content = {
+        
+        title : 'Article one Anu Tony',
+        heading : 'Article one',
+        date : 'Sep 5,2016',
+        content : 
+                    `<p> This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
+                         		
+                     </p>		
+                     <p>		
+                         This is article 2..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
+                        		
+                     </p>		
+                     <p>		
+                         This is article three .`.`.``````````````````````````````````````````````````````````````````````````is is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..
+                         </p>`
+                         
+    };
     
-    title : 'Article one Anu Tony',
-    heading : 'Article one',
-    date : 'Sep 5,2016',
-    content : 
-                `<p> This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
-                     		
-                 </p>		
-                 <p>		
-                     This is article 2..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
-                    		
-                 </p>		
-                 <p>		
-                     This is article three .`.`.``````````````````````````````````````````````````````````````````````````is is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..
-                     </p>`
-                     
-};
-
-var htmltemplate = {`
-    <html>		
-     <head>		
-        <title>		
-            ${title}		
-        </title>
-    <meta name="viewport" content="width=device-width">
-    <link href="/ui/style.css" rel="stylesheet" />     
-    </head>		
-         <body>	
-         <div class = "container">
-             <div>		
-                <a href="/">Home</a>		
-             </div>		
-             		
-             <h3>		
-                 ${heading}		
-             </h3> 		
-             <div>		
-                ${date}		
-             </div>		
-             <div>		
-                ${content}		
-             </div>		
-          	</div>	
-         </body>     		
- </html>`
-
-};
+    function CreateTemplate (data) { 
+    var title = data.title;
+    var date = data.date;
+    var heading =  data.heading;
+    var content = data.content;
+    var htmltemplate = 
+        `<html>		
+         <head>		
+            <title>		
+                ${title}		
+            </title>
+        <meta name="viewport" content="width=device-width">
+        <link href="/ui/style.css" rel="stylesheet" />     
+        </head>		
+             <body>	
+             <div class = "container">
+                 <div>		
+                    <a href="/">Home</a>		
+                 </div>		
+                 		
+                 <h3>		
+                     ${heading}		
+                 </h3> 		
+                 <div>		
+                    ${date}		
+                 </div>		
+                 <div>		
+                    ${content}		
+                 </div>		
+              	</div>	
+             </body>     		
+     </html>`;
+     
+    return htmltemplate
+    }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(CreateTemplate(articleOne.html));
 });
 
 app.get('/article-three', function (req, res) {
