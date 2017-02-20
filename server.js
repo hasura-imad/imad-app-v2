e509f0015b13756779d71d0f51e758b0b190f8ff
anutony <aisavaria@gmail.com>
2017-02-20 15:39:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ var content = {
                         		
                      </p>		
                      <p>		
-                         This is article three .`.`.``````````````````````````````````````````````````````````````````````````is is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..
+                         This is article three .``````````````````````````````````````````````````````````````````````````is is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..
                          </p>`
-                         
+                    
     };
     
     function CreateTemplate (data) { 
