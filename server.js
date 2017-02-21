6ea3dbaa84fbc5b51dbc651bf1ab1745009a4f11
anutony <aisavaria@gmail.com>
2017-02-21 20:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleName', function (req, res){
-res.send(createTemplate(articles[articleName]));
 var articleName = req.params.articleName;
+res.send(createTemplate(articles[articleName]));
+
 });
 
 
