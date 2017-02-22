3836ed53f6a183254e9493dd39ed23dc9e287f15
anutony <aisavaria@gmail.com>
2017-02-22 17:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,45 @@
 console.log('Loaded!');
+
+
+var articles = {
+    `articleOne` : {
+           
+        title   : 'Article one !Anu Tony',
+        heading : 'Article one',
+        date    : 'Sep 10,2016',
+        content : 
+                   ` <p> This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
+                         		
+                     </p>		
+                     <p>		
+                         This is article1..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..		
+                        		
+                     </p>		
+                     <p>		
+                         This is article 1 is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..This is article one..
+                         </p>`
+                    
+                 },
+   `articleTwo`     : {
+             
+        title : 'Article three  !Anu Tony',
+        heading : 'Article three ',
+        date : 'oct12,2016',
+        content : 
+                   ` <p> This is article three.This is article threeThis is article threeThis is article threeThis is article threeThis is article three
+                        </p>`
+                    
+    },
+        
+        
+    `articleThree`     : {
+             
+        title : 'Article three  !Anu Tony',
+        heading : 'Article three ',
+        date : 'oct12,2016',
+        content : 
+                   ` <p> This is article three.This is article threeThis is article threeThis is article threeThis is article threeThis is article three
+                        </p>`
+                    
+    }
+                };
