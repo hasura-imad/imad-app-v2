ef1492522f1e5dc49d6673a5d490b1f4265ac37b
Shristy <sahuwalashristy@gmail.com>
2017-02-23 11:08:08 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ element.innerHTML= "New Value";
 var img=document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft=marginLeft + 3;
+    marginLeft=marginLeft + 1;
     img.style.marginLeft = marginLeft + "px";
 }
 img.onclick = function() {
