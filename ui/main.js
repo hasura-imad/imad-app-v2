094ea5e00b6fd84e9ae4b431eebe75fd9b3d56fa
Shristy <sahuwalashristy@gmail.com>
2017-02-23 10:42:43 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ console.log('Loaded!');
 //Change the text of the main-text element
 var element = document.getElementById('main-text');
 element.innerHTML= "New Value";
+
+//Change the text of the main-text element
+var element = document.getElementById('main-text');
+element.innerHTML= "New Value";
+
+//Move the image
+var img=document.getElementById('madi');
+img.onclick = function() {
+  img.style.marginLeft = '100px';  
+};
