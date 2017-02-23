66f4def921d705d368c057be3da5423eb3e1af4d
Shristy <sahuwalashristy@gmail.com>
2017-02-23 10:18:39 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
     res.sendFile(path.join(_dirname, 'ui', 'main.js'));
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
