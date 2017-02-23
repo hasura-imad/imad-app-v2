4e85eaee834d9fe5cb8f4128e931d2c830062a35
Shristy <sahuwalashristy@gmail.com>
2017-02-23 11:08:53 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,5 @@ function moveRight(){
     img.style.marginLeft = marginLeft + "px";
 }
 img.onclick = function() {
-  var interval = setInterval(moveRight, '50px'); 
+  var interval = setInterval(moveRight, 50); 
 };
