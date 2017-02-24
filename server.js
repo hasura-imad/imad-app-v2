5f9d068bc78e6ad2942148bf4e07c76a9ae6d755
Bhargav Vasist <bhargavclick@gmail.com>
2017-02-24 21:38:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,8 +77,12 @@ var htmlTemplate = `
 `;
 return htmlTemplate;
 }
-    
-
+    var counter=0;
+app.get('/counter',function (req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+}
+);
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
