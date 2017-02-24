a03ff207a1e62fe15a7c4c14af1610a80ee0cae9
Bhargav Vasist <bhargavclick@gmail.com>
2017-02-24 21:54:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,22 @@
-var counter=0;
+
 var butt = document.getElementById('counter');
 
 butt.onclick = function () {
     
+    var request= new XMLHttprequest();
+    request.onreadystatechange = function(){
+        if(request.readyState==XMLHttprequest.DONE)
+        {
+            if(request.status==200){
+              var counter =  request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+            }
+        }
+        
+        
+    };
     
-    
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.open('GET','http://bhargav-vasist.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
