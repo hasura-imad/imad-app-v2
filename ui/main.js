5f856a3b6bd85de0b1d8e26a87a446e470706812
Shristy <sahuwalashristy@gmail.com>
2017-02-26 07:21:08 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,24 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function(){
-  
-  //Make a request to the counter endpoint
-  
-  //Capture the response and store it in a variable
-  
-  //Render the variable in the correct span
-  counter = counter + 1;
-  var span=document.getElementById('count');
-  span.innerHTML = counter.toString();
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    };
+ 
+    //Make a request
+    request.open('GET', 'http://iamshristy.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
