f4763cff3c344c8fac02feb76c750ec6d5bc2a1c
anutony <aisavaria@gmail.com>
2017-02-26 06:59:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,13 @@ console.log('Loaded!');
 
 
 //move the img
+
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
-marginLeft = marginLeft + 'px';
+marginLeft = marginLeft + 1;
+img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100)
+    var interval = setInterval(moveRight, 50)
 };
