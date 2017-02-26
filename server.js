759c3ae5001793621c3a4c93e0475e32dadc5917
Shristy <sahuwalashristy@gmail.com>
2017-02-26 06:57:12 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,14 +79,14 @@ function createTemplate (data){
     return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 app.get('/:articleName',function (req, res) {
     //articleName = article-one
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
