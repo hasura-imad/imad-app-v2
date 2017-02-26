ea21a1278540d56d1b1caa5c5a0e52717ca6e997
Shristy <sahuwalashristy@gmail.com>
2017-02-26 06:53:47 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter=0;
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+   res.send(counter.toString()); 
+});
+
 var articles= {
     'article-one': {
         title: 'Article One | Shristy',
