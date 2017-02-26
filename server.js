f520f6b7e57ba2a3f3f7d7538c2ab0c543fe8490
Shristy <sahuwalashristy@gmail.com>
2017-02-26 08:26:29 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get('/counter', function(req, res){
    res.send(counter.toString()); 
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req, res){
+   //Get the name from the request
+   var name= req.params.name;
+   
+   names.push(name);
+   //JSON : JavaScript Object Notation
+   res.send(JSON.stringify(names));
+});
+
 var articles= {
     'article-one': {
         title: 'Article One | Shristy',
