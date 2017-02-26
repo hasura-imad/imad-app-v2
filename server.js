56e85823af8fccb9773ae44cf2807bee1447a8c5
Shristy <sahuwalashristy@gmail.com>
2017-02-26 08:30:12 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.get('/counter', function(req, res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name', function(req, res){ //URL : /submit-name?name=xxxxx
    //Get the name from the request
-   var name= req.params.name;
+   var name= req.query.name;
    
    names.push(name);
    //JSON : JavaScript Object Notation
