43a5e549cec7386b79c65313131a8a77f79bf484
zercius <zercius1@gmail.com>
2017-03-17 20:45:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/sample',function(req,res){
-    res.sendFile(path.join(_dirname,'/','sample.html'));
+    res.sendFile(path.join(_dirname,'ui','sample.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
