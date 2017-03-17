d78d169008cc0a1f1e9faa81729d3fa81b3b8a1a
zercius <zercius1@gmail.com>
2017-03-17 19:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/ui/index2.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
 });
 
