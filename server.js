a41f8a3c0dcb3c8d821fb9aa729e410647fb1ef3
Siddhant Tiwari <tiwari.siddhant.ce@gmail.com>
2017-03-23 12:26:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article-one', function(req, res) {
+app.get('/article-one', function(req, res) {
   res.send('Article one was requested and will be served here.'); 
 });
 
-app.get('article-two', function(req, res) {
+app.get('/article-two', function(req, res) {
   res.send('Article two was requested and will be served here.'); 
 });
 
-app.get('article-three', function(req, res) {
+app.get('/article-three', function(req, res) {
   res.send('Article three was requested and will be served here.'); 
 });
 
