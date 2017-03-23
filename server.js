227046eb1ed72cc9ecbbe5cbcd3394121dad6864
Siddhant Tiwari <tiwari.siddhant.ce@gmail.com>
2017-03-23 13:20:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/content/article-one', function(req, res) {
+  res.sendFile(path.join(__dirname, 'content', 'article-one.html'));
 });
 
-app.get('/article-two', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
+app.get('/content/article-two', function(req, res) {
+  res.sendFile(path.join(__dirname, 'content', 'article-two.html')); 
 });
 
-app.get('/article-three', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+app.get('/content/article-three', function(req, res) {
+  res.sendFile(path.join(__dirname, 'content', 'article-three.html')); 
 });
 
 app.get('/ui/style.css', function (req, res) {
