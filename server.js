c88cb42151917bacfbf1f61473ea293c91cff372
Jinesh220 <jsubhashchand31@student.mes.ac.in>
2017-07-05 23:05:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool=require('pg').Pool;
-var crypto=require('crypto');
-var bodyParser = require('bodyparser');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var session = require('express-session');
-
-
 
 var config = {
     user: 'jinesh220',
     database: 'jinesh220',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: db-jinesh220-47751
+    password:db-jinesh220-47751
 };
-
-
 
 var app = express();
 app.use(morgan('combined'));
@@ -25,6 +21,7 @@ app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
+
 
 function createTemplate (data) {
     var title = data.title;
@@ -71,8 +68,6 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -83,6 +78,7 @@ function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
+
 
 app.get('/hash/:input', function(req, res) {
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
@@ -106,58 +102,9 @@ app.post('/create-user', function (req, res) {
    });
 });
 
-app.post('/login', function (req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
-          } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                
-                res.send('credentials correct!');
-                
-              } else {
-                res.status(403).send('username/password is invalid');
-              }
-          }
-      }
-   });
-});
 
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
 
-app.get('/logout', function (req, res) {
-   delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
-});
+
 
 var pool = new Pool(config);
 
@@ -237,52 +184,7 @@ app.get('/ui/:fileName', function (req, res) {
 });
 
 
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-
-
-
-
-
-
-
-
-
-
-/*
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-    
-  res.send(counter.toString());
-});
-
-
-
-
-
-app.get('/article-one',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
-});
-app.get('/article-three',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));  
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});*/
